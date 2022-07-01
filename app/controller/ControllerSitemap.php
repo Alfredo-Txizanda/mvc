d42668555ec6dc26d08ce9f0c2c9c928b0f6386e
Alfredo-Txizanda <txizanda.treinamento@gmail.com>
2022-07-01 15:29:26 +0100

    teste

--- app/controller/ControllerSitemap.php
+++ app/controller/ControllerSitemap.php
@@ -6,4 +6,8 @@
         public function testeMetodo() {
             echo "<h1>Este método existe!</h1>";
         }
+
+        public function teste1() {
+            echo "<h2>Método teste 2</h2>";
+        }
     }
